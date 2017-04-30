f2719bb77aa4f5f7b30ae9d905f670c55a0df616
Pavel <pavel.oliynyk@gmail.com>
2017-04-30 12:31:38 +0100

    Update Extraction logic

--- observatory/src/main/scala/observatory/Extraction.scala
+++ observatory/src/main/scala/observatory/Extraction.scala
@@ -7,13 +7,26 @@ import java.time.LocalDate
   */
 object Extraction {
 
+
+  def fahrenheitToCelsius(f : Double): Double = {
+
+    val result : Double = (f - 32.toDouble) * ( 5.toDouble / 9.toDouble )
+
+    BigDecimal( result ).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+  }
+
+
   /**
     * @param year             Year number
     * @param stationsFile     Path of the stations resource file to use (e.g. "/stations.csv")
     * @param temperaturesFile Path of the temperatures resource file to use (e.g. "/1975.csv")
     * @return A sequence containing triplets (date, location, temperature)
     */
+
+
   def locateTemperatures(year: Int, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Double)] = {
+
+
     ???
   }
 
--- observatory/src/test/scala/observatory/ExtractionTest.scala
+++ observatory/src/test/scala/observatory/ExtractionTest.scala
@@ -7,5 +7,23 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class ExtractionTest extends FunSuite {
 
-  
+  test("when 0F: should return -17.78C")  {
+    val zero : Double = 0.toDouble
+    val result : Double = Extraction.fahrenheitToCelsius(zero)
+    assert(result === -17.78)
+  }
+
+  test("when 10F: should return -12.22C")  {
+    val f : Double = 10
+    val result = Extraction.fahrenheitToCelsius(f)
+    assert(result === -12.22)
+  }
+
+
+  test("when 20F: should return -6.67C")  {
+    val f : Double = 20
+    val result = Extraction.fahrenheitToCelsius(f)
+    assert(result === -6.67)
+  }
+
 }
